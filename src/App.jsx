0e41b186f2a8d58c1ca438258fea43cf7f930edb
Amrut Patil <patilamrut578@gmail.com>
2025-01-23 14:49:48 +0530

    Publish to GItHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Vans from './Pages/Vans';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <header>
+        <Link to="/" className='site-logo' >#VanCamp</Link>
+        <nav>
+          <Link to="/about" >About</Link>
+          <Link to="/vans" >Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className="home-container">
+        <h1>You got the travel plans, we got the travel bans.</h1>
+        <p>Add adventure to your life by joining the #VanCamp movement. Rent the perfect van to make your perfect road trip.</p>
+        <Link to='vans'>Find your van</Link>
+    </div>
+  )
+}
+
+export default Home
