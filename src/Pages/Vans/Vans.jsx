80543a3bc927829a1ff091a32a557074cf5a6d51
Amrut Patil <patilamrut578@gmail.com>
2025-01-25 19:22:26 +0530

    Add searchParams

--- src/Pages/Vans/Vans.jsx
+++ src/Pages/Vans/Vans.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 const Vans = () => {
   const [vans, setVans] = useState([])
+  const [searchParams, setSearchparams] = useSearchParams()
+
+  const typeFilter = searchParams.get("type")
+  
+  
 
   useEffect(() => {fetch("/api/vans")
     .then(res => res.json())
     .then(data => setVans(data.vans))
   }, [])
+
 
   const vanElements = vans.map(van => (
     <div key={van.id} className='van-card-container'>
@@ -22,10 +28,26 @@ const Vans = () => {
     </div>
   ))
 
+  const vanTypes = [...new Set(vans.map(van => van.type))]
+  const typeFilterButtons = vanTypes.map(type => 
+    <div key={type} className='filter-buttons-container'>
+      <button 
+        className={`van-type ${typeFilter === type && type} filter-button`}
+        onClick={() => setSearchparams({type: type})}
+      >{type}</button>
+    </div>
+  )
+
   return (
     <div className='vans-list-container'>
       <h1>Explore our van options</h1>
-      <div className='vans-list'>
+      {typeFilterButtons}
+      {typeFilter && 
+        <button 
+          className='van-type filter-button'
+          onClick={() => setSearchparams({})}
+        >Clear filter</button>}
+      <div className='vans-list filter-button'>
         {vans.length > 0? vanElements: <h1>Loading...</h1>}
       </div>
     </div>
