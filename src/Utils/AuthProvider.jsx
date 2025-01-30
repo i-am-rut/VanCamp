c9cfbbaae5609513aab02a47f69a1b6179742caf
Amrut Patil <patilamrut578@gmail.com>
2025-01-31 00:51:12 +0530

    Add potential fix to login logout problem

--- src/Utils/AuthProvider.jsx
+++ src/Utils/AuthProvider.jsx
@@ -1,44 +1,44 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import axios from "axios";
 
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+    // const [isLoading, setIsLoading] = useState(true);
     
-    useEffect(() => {
-        checkLogin();
-    }, []); 
+    // useEffect(() => {
+    //     checkLogin();
+    // }, []); 
 
-    const checkLogin = async () => {
-        try {
-            const res = await axios.get("https://vancamp-backend.onrender.com/api/auth/me", { withCredentials: true });
-            setUser(res.data.user);
-        } catch (error) {
-            setUser(null);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+    // const checkLogin = async () => {
+    //     try {
+    //         const res = await axios.get("https://vancamp-backend.onrender.com/api/auth/me", { withCredentials: true });
+    //         setUser(res.data.user);
+    //     } catch (error) {
+    //         setUser(null);
+    //     } finally {
+    //         setIsLoading(false);
+    //     }
+    // };
 
     const login = async (email, password) => {
         try {
-            await axios.post("https://vancamp-backend.onrender.com/api/auth/login", {email, password}, {withCredentials: true}, {headers : {'Content-Type': 'application/x-www-form-urlencoded'}})
-            checkLogin()
+            const res = await axios.post("https://vanlife-backend.onrender.com/api/auth/login", {email, password}, {withCredentials: true}) 
+            setUser(res.data)
+            // checkLogin()
         } catch (error) {
             throw new Error(error.response?.data?.error || "Login failed")
         }
     }
 
     const logout = async () => {
-            await axios.post("https://vancamp-backend.onrender.com/api/auth/logout", {}, {withCredentials: true})
-            setUser(null)
-        
+        await axios.post("https://vancamp-backend.onrender.com/api/auth/logout", {}, {withCredentials: true})
+        setUser(null)
     }
 
     return (
-        <AuthContext.Provider value={{ user, isLoading, login, logout}}>
+        <AuthContext.Provider value={{ user, login, logout}}>
             {children}
         </AuthContext.Provider>
     )
